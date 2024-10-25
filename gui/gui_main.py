823892727cf0a71fdef19cd16b294273e8a848b2
seanvsmith1901 <168485841+seanvsmith1901@users.noreply.github.com>
2024-10-25 14:37:46 -0600

    fake player can now move
    fake player can now move. There's a couple of questions I have for ethan that I should probably ask him
    Only problem is I don't want him to realize how big of an idiot I am lol

--- gui/gui_main.py
+++ gui/gui_main.py
@@ -2,7 +2,9 @@ from operator import truediv
 
 import pygame
 from pygame import K_ESCAPE
+from environment.runner import run
 
+# i know for a fact that I need at least these 2.
 # i know for a fact that I need at least these 2.
 import environment
 import agents
@@ -15,6 +17,8 @@ from pygame.locals import ( # gets us the four caridnal directions for movement 
     K_RIGHT,
 )
 
+
+
 SCREEN_WIDTH = 800 # fetch it we are making it a square.
 SCREEN_HEIGHT = 800
 
@@ -22,15 +26,43 @@ human_player = pygame.Surface((20,20)) # creates a little player object
 human_player.fill((0,120,120))
 human_rect_text = human_player.get_rect()
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+    def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 5)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-5, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(5, 0)
+
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+
 
 
 pygame.init() # actually starts the game.
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # not sure what the preferred game size is but lets start there IG.
 
+player = Player()
 
 running = True
 while running:
     for event in pygame.event.get():
+        pressed_keys = pygame.key.get_pressed()
+
         if event.type == pygame.QUIT:
             running = False
 
@@ -38,6 +70,7 @@ while running:
             if event.key == K_ESCAPE: #gives us a way to stop execution.
                 running = False
             # now we need to pass off the movement.
+            #
 
         # need to handle 4 events
         #1. process user input
@@ -45,10 +78,9 @@ while running:
         #3. updates the display (and audio output)
         #4. maintains the speed of the game.
 
-    screen.fill((255,255,255))
-
-    pygame.draw.circle(screen, (0,0,255), (400, 400), 75)
-
-    pygame.display.update()
+        screen.fill((0,0,0))
+        screen.blit(player.surf, player.rect)
+        player.update(pressed_keys)
+        pygame.display.flip()
 
 pygame.quit()
